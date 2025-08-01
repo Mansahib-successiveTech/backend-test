61f38b1ac62d39d399be68b54279a74f02118c82
Mansahib Singh <mansahib.singh@mansahibvrs16.successivetech.com>
2025-08-01 12:55:22 +0530

    changes made

--- src/controller/getStudents.ts
+++ src/controller/getStudents.ts
@@ -2,9 +2,14 @@ import { Request, Response } from "express";
 import { students } from "../models/students";
 
 export const getall=async(req:Request,res:Response)=>{
+ const {limit}=req.query || 10 ;
+ const {page}=req.query ;
+
+  const skip=parseInt(page-1)*parseInt(limit)
+
 
 try{
-const result=await students.find();
+const result=await students.find().sort({name:1}.skip(skip).limit(limit));
 return res.status(200).json({
     data:result
 })
